e53b6760d100797dddc21b5269d01499451cbb31
Arda Cinar <kuzux92@gmail.com>
2022-04-02 23:34:31 +0300

    Styling the main page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { useEffect, useState } from 'react'
-import { Item } from '../lib/stuff'
-import styles from '../styles/Home.module.css'
-import { za } from '/lib/stuff'
+import { Item } from '/lib/stuff'
 
 export default function Home(props) {
   const [items, setItems] = useState(props.items);
@@ -81,33 +77,47 @@ export default function Home(props) {
 
   const itemElems = items.map((item) => {
     let button;
+    let checkbox;
     
     if(item.status === 0) button = <>
-      <button onClick={() => changeStatus(item.id, 1)} disabled={item.loading}>Complete</button>
-      <button onClick={() => changeStatus(item.id, 2)} disabled={item.loading}>Ignore</button>
+      <button onClick={() => changeStatus(item.id, 2)} disabled={item.loading}>❌</button>
     </>;
 
-    return <li key={item.id}>
-      {item.name}
-      {button}
-    </li>;
+    if(item.status === 0 || item.status === 1) {
+      checkbox = <input type="checkbox" checked={item.status === 1} onChange={ evt => {
+        let newStatus = 0;
+        if(evt.target.checked) newStatus = 1;
+        changeStatus(item.id, newStatus);
+      }} />;
+    }
+    let classes = ["grid", "grid-cols-3", "gap-4"];
+    if(item.status === 1 || item.status === 2) {
+      classes.push('text-gray-600');
+    }
+    if(item.status === 1) {
+      classes.push('line-through');
+    }
+
+    return <div key={item.id} className={classes.join(' ')}>
+      <div className="text-right">{checkbox}</div>
+      <div>{item.name}</div>
+      <div>{button}</div>
+    </div>;
   });
 
-  return (
-    <div className={styles.container}>
-      <ul>
+  return (<div className="grid place-items-center">
+      <div className="shadow border rounded w-6/12 mb-4 py-4 bg-white">
         {itemElems}
-      </ul>
-      <p>{props.za}</p>
+      </div>
+
       <form onSubmit = {createItem}>
-        <input name="name" type="text" required />
-        <button type="submit">Create New Item</button>
+        <input name="name" className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" required />
+        <button type="submit" className="bg-white hover:bg-pink-500 text-pink-500 hover:text-white font-bold py-2 px-4 border border-pink-700 rounded">Create New Item</button>
       </form>
-    </div>
-  )
+  </div>);
 }
 
 export async function getServerSideProps(context) {
   const items = await Item.findAll({ raw: true });
-  return { props: { items, za } };
+  return { props: { items } };
 }
